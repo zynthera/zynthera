7e190105716f5b9e4aa906eba79787f5982a1393
Zynthera <206341821+zynthera@users.noreply.github.com>
2025-05-05 12:11:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 import { SecurityProvider } from "@/components/SecurityProvider";
 import { SecurityHelmet } from "@/components/SecurityHelmet";
 import Index from "./pages/Index";
@@ -23,7 +22,7 @@ const App = () => (
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              {/* Catch-all route added to handle unknown paths */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
